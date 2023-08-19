1be6890b88ac3eac0736780bd978ba6a94e27503
Vivek Yadav <122459390+vivek5312@users.noreply.github.com>
2023-08-19 11:36:30 +0530

    Add loading functionality

--- src/App.js
+++ src/App.js
@@ -1,75 +1,42 @@
 import React, { useState } from 'react';
+import MoviesList from './components/MoviesList';
 import './App.css';
-import data from './components/Back/Data/Data';
-import Header from './components/Font/Header/Header';
-import Product from './components/Font/Product';
-import Cart from './components/Font/Cart/Cart';
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
-import About from './components/Font/Header/Pages/About/About';
-import Home from './components/Font/Header/Pages/Home/Home';
 
 function App() {
-  const { productItems } = data;
-  const [cartShown, setCartShown] = useState(false);
-  const [cartItem, setCartItem] = useState([]);
+  const [movies, setMovies] = useState([]);
+ const [isLoading,setLoading]=useState(false);
+  async function fetchMovieshandler() {
+    try {
+      setLoading(true);
+      const response = await fetch('https://swapi.dev/api/films');
+      const data = await response.json();
 
-  const shownCartHandler = () => {
-    setCartShown(true);
-  };
+      const transformMovies = data.results.map((moviesData) => ({
+        id: moviesData.episode_id,
+        title: moviesData.title,
+        openingText: moviesData.opening_crawl,
+        releaseDate: moviesData.release_date,
+      }));
 
-  const hiddenCartHandler = () => {
-    setCartShown(false);
-  };
-
-  const handleAddProduct=(product)=>{
-    const ProductExist=cartItem.find((item)=>item.id===product.id);
-    if(ProductExist){
-      setCartItem(cartItem.map((item)=>item.id===product.id ? {...ProductExist,quantity:ProductExist.quantity+1}:item))
-    }
-   else{
-    setCartItem([...cartItem,{...product,quantity:1}])
-   }
-  }
-
-  const handleRemoveProduct=(product)=>{
-    const ProductExist=cartItem.find((item)=>item.id===product.id);
-    if(ProductExist.quantity===1){
-      setCartItem(cartItem.filter((item)=>item.id!==product.id));
-
-    }
-    else{
-      setCartItem(cartItem.map((item)=>item.id===product.id?{...ProductExist,quantity:ProductExist.quantity-1}:item));
+      setMovies(transformMovies);
+      setLoading(false);
+    } catch (error) {
+      console.error('Error fetching movies:', error);
     }
   }
-const handleCartClearance=()=>{
-  setCartItem([]);
-}
 
   return (
-    <div className="App">
-   <Router>
-   <Header onShown={shownCartHandler} cartItem={cartItem} />
-     <Routes>
-      <Route path='/' element={   <Product productItems={productItems}  handleAddProduct={handleAddProduct}/>}/>
-      <Route path="/about" element= {<About/>}/>
-      <Route path='/home' element={<Home/>}/>
-     </Routes>
-  
-  
-  
-
-
-      {cartShown && <Cart 
-      onhidden={hiddenCartHandler}
-      cartItem={cartItem} 
-      handleAddProduct={handleAddProduct}
-      handleRemoveProduct={handleRemoveProduct}
-      handleCartClearance={handleCartClearance}
-      />}
-      </Router>
-    </div>
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMovieshandler}>Fetch Movies</button>
+      </section>
+      <section>
+        {!isLoading && <MoviesList movies={movies} />}
+        {!isLoading && movies.length===0 && <p>No movies found</p>}
+        {isLoading && <p>Loading...</p>}
+      </section>
+    </React.Fragment>
   );
 }
-
 
 export default App;
