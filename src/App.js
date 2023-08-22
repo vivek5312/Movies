5da5668fd3a51df974f90298ea70f2a82a9118c7
Vivek Yadav <122459390+vivek5312@users.noreply.github.com>
2023-08-22 16:02:13 +0530

    using useEffect for fetch api

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React, { useState, useEffect } from 'react';
+
+import React, { useEffect, useState,useCallback } from 'react';
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [isLoading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const [retryCount, setRetryCount] = useState(0);
-  const [retrying, setRetrying] = useState(false);
+ const [isLoading,setLoading]=useState(false);
+ const [error,setError]=useState(null);
 
-  async function fetchMovieshandler() {
+
+
+
+  const fetchMovieshandler=useCallback(async ()=>{ 
     try {
       setLoading(true);
-      setError(null);
-      const response = await fetch('https://swapi.dev/api/film');
-
-      if (!response.ok) {
+      const response = await fetch('https://swapi.dev/api/films') 
+    
+      if(!response.ok){
         throw new Error('Something went wrong ....Retrying');
       }
       const data = await response.json();
@@ -28,46 +29,26 @@ function App() {
       }));
 
       setMovies(transformMovies);
+      
+      setError()
     } catch (error) {
-      setError(error.message);
-      setRetrying(true);
-      setRetryCount(retryCount + 1);
-    } finally {
-      setLoading(false);
+      setError(error.message)
     }
-  }
+    setLoading(false);
+  },[]);
 
-  useEffect(() => {
-    let retryInterval;
-
-    if (retrying && retryCount < 5) {
-      retryInterval = setInterval(() => {
-        fetchMovieshandler();
-      }, 5000); // Retry every 5 seconds
-    } else {
-      clearInterval(retryInterval);
-      setRetrying(false);
-      setRetryCount(0);
-    }
-
-    return () => {
-      clearInterval(retryInterval);
-    };
-  }, [retrying, retryCount]);
-
+  useEffect(()=>{
+    fetchMovieshandler();
+   },[fetchMovieshandler])
+ 
   return (
     <React.Fragment>
       <section>
-        <button onClick={fetchMovieshandler} disabled={retrying}>
-         Fetch Movies
-        </button>
-        {retrying && (
-          <button onClick={() => setRetrying(false)}>Cancel</button>
-        )}
+        <button onClick={fetchMovieshandler}>Fetch Movies</button>
       </section>
       <section>
         {!isLoading && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && !error && <p>No movies found</p>}
+        {!isLoading && movies.length===0 && !error && <p>No movies found</p>}
         {!isLoading && error && <p>{error}</p>}
         {isLoading && <p>Loading...</p>}
       </section>
